bb3690fa8a16233871ec6eb4a7bba7cb5e6cd20a
hoshino <hoshino.satoshi@fujitsu.com>
2026-05-28 14:52:02 +0900

    base snapshot: b1ba5428513b52c2bd6acfd3ad0a910f699bc395

---
+++ tools/perf/perf.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _PERF_PERF_H
+#include <stdbool.h>
+
+#define _PERF_PERF_H
+
+#define MAX_NR_CPUS			4096
+
+enum perf_affinity {
+	PERF_AFFINITY_SYS = 0,
+	PERF_AFFINITY_NODE,
+	PERF_AFFINITY_CPU,
+	PERF_AFFINITY_MAX
+};
+
+#endif
